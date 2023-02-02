9b3c4f6dfa696e97d03c97a25f5e56766a88280c
devinnihbos <118157340+devinnihbos@users.noreply.github.com>
2023-02-02 20:32:36 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="fixed-bottom">
+        <footer class="p-3 bg-primary text-light d-flex justify-content-center"><p>Copyright &copy; <b><a class="text-warning"href="https://instagram.com/devinfirmansyah_">@devingantengbanget</a></b> All right reserved.</p></footer>
+    </div>
